5761a6107df68a9a3307bb9c56a7d6f9de6c4e41
SennaNetwork <sennaNetwork@outlook.com>
2022-05-20 12:14:48 +0800

    deploy

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = 'https://gist.githubusercontent.com/SennaNetwork/63694d9b072a698b55d6dcd084013c46/raw/34b4c189d082d79c470a526b13548daa451e8066/tokens.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
